2b5b8768b8db12a50af127c67caa5d9108b7eab6
Yui Chun Leung <leo.leung@faculty.ai>
2022-11-07 14:38:10 +0000

    successfully twine upload to pypi

--- setup.py
+++ setup.py
@@ -4,4 +4,8 @@ with open("requirements.txt") as f:
     required = f.read().splitlines()
 
 if __name__ == "__main__":
-    setup(use_scm_version=True, install_requires=required, test_suite="test")
+    setup(
+        use_scm_version={"local_scheme": "no-local-version"},
+        install_requires=required,
+        test_suite="test",
+    )
