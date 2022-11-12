2a8754845e84be012c5062ba8743ef0fde977703
Yui Chun Leung <leo.leung@faculty.ai>
2022-11-12 22:14:26 +0000

    add new method

--- src/pypack_guide/utils.py
+++ src/pypack_guide/utils.py
@@ -1,2 +1,6 @@
 def add(num1: int, num2: int) -> int:
     return num1 + num2
+
+
+def subtract(num1: int, num2: int) -> int:
+    return num1 - num2
--- test/pypack-guide/test_utils.py
+++ test/pypack-guide/test_utils.py
@@ -1,6 +1,9 @@
-from pypack_guide.utils import add
+from pypack_guide.utils import add, subtract
 
 
 class Test_Utils:
     def test_add(self):
         assert add(1, 2) == 3
+
+    def test_subtract(self):
+        assert subtract(3, 1) == 2
